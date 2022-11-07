78fe2a8f724879f4c54570fa01c8fde0f378fbde
Alec M <apmaster505@gmail.com>
2022-11-07 00:22:16 -0700

    :construction_worker: Added directoy detection & conditional render
    Main entry with a conditional rendering statement to adhere to git functions when signed in vs not signed in. Both will take different paths upon options selected.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const importJsx = require('import-jsx');
-const {render, measureElement, Box, Text} = require('ink');
-const { useEffect } = require('react');
+const {render, measureElement, Box, Text, Newline} = require('ink');
+const { useEffect, useState } = require('react');
+const { useCurrentDirectoryBase, useDirectoryExists } = require('./hooks/useFileExplorer');
 const App = importJsx('./components/ui');
-const CommitList = importJsx('./components/CommitList');
+const BigText = require('ink-big-text');
+const Gradient = require('ink-gradient');
 
 function Main(props) {
-
 	// Let each option depict view and other options needed to complete user interaction.
 	let flagOptions = {
 		'name': Boolean,
@@ -16,6 +17,9 @@ function Main(props) {
 		'login': Boolean,
 		'logout': Boolean,
 		'list': Boolean,
+		'link': () => {
+
+		},
 		'l': 'list',
 		'version': Boolean,
 		'v': 'version',
@@ -23,6 +27,8 @@ function Main(props) {
 		'create': Boolean,
 		'c': 'create',
 	}
+	const [gitDirectoryExists, setGitDirectoryExists] = useState(false)
+
 
 	useEffect(() => {
 		for (const option in props) {
@@ -30,6 +36,9 @@ function Main(props) {
 
 			console.log('Chosen options:', optionChosen)
 		}
+
+		setGitDirectoryExists(useDirectoryExists('.git'))
+
 	}, [])
 
 	console.log('Props:', props)
@@ -38,10 +47,32 @@ function Main(props) {
 	// TODO: Map version, date to markdown file.
 	// TODO: Map views and logic to command flags
 
+	function renderGitOptions() {
+		if (gitDirectoryExists) {
+			// Render inquirer
+			return (
+				<Box borderColor="green" borderStyle="round" paddingX={2}>
+					<Text>Git repo <Text color='green'>exists</Text>! ✔ </Text>
+				</Box>
+			)
+		}
+
+		return (
+			<Box alignItems="flex-start">
+				<App/>
+			</Box>
+		)
+	}
+
 	return (
-		<Box>
-			<App/>
-		</Box>
+		<>
+			<Box>
+				<Gradient name='atlas'>
+					<BigText font='tiny' text="Chamoji"/>
+				</Gradient>
+			</Box>
+			{renderGitOptions()}
+		</>
 	);
 }
 
